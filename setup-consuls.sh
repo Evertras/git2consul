74dcdd38d9941e3da501ba14718fcd5b45ce45c8
Brandon Fulljames <bfulljames@blizzard.com>
2018-01-26 18:00:36 -0800

    Use tokens and update consul lib

--- setup-consuls.sh
+++ setup-consuls.sh
@@ -2,6 +2,7 @@
 
 docker stop consul1 consul2 &>/dev/null
 
-docker run -d --rm --name=consul1 -p 8501:8500 consul agent -dev -client 0.0.0.0
-docker run -d --rm --name=consul2 -p 8502:8500 consul agent -dev -client 0.0.0.0
+OPTIONS='-dev -client 0.0.0.0'
 
+docker run -d --rm -v ${PWD}/acl-config.json:/consul/config/acl-config.json:ro --name=consul1 -p 8501:8500 consul agent $OPTIONS
+docker run -d --rm -v ${PWD}/acl-config.json:/consul/config/acl-config.json:ro --name=consul2 -p 8502:8500 consul agent $OPTIONS
